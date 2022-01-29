61b2951abf8a5196477c0e54a889356093d3ddce
aakrut dabhi <aakrutdabhi@gmail.com>
2022-01-29 13:08:01 +0530

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Navbar,Exchanges,Homepage,Cryptocurrencies,CryptoDetails,News } from './components'
 import './App.css';
+import { Layout, Typography, Space } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />}></Route>
+                <Route path="/exchanges" element={<Exchanges />}></Route>
+                <Route
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                ></Route>
+                <Route path="/crypto/:id" element={<CryptoDetails />}></Route>
+                <Route path="/news" element={<News />}></Route>
+              </Routes>
+            </div>
+          </Layout>
+
+          <div className="footer">
+            <Typography.Title
+              level={5}
+              style={{ color: "white", textAlign: "center" }}
+            >
+              Cryptogram <br />
+              All rights reserverd.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
+        </div>
+      </div>
+    );
+};
 
 export default App;
---
+++ src/components/CryptoDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CryptoDetails = () => {
+  return <div>Cryptodeatils</div>;
+};
+
+export default CryptoDetails;
